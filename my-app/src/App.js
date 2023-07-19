467385d5cecd988724238afd15cd761c66bcb3ee
K-JC <kellytolfts93@hotmail.com>
2023-07-19 12:30:34 +0000

    add positems js file and create postitem function, title, name, image

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
-import css from './App.module.css';
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
-import NavBarSimple from './components/NavBarSimple';
+import NavBarForm from "./components/NavBarForm";
 
 function App() {
   return (
     <div className={css.App}>
-      <Sidebar/>
-      <NavBarSimple/>
+      {/* Add your components here */}
+      <NavBarForm />
+      <Sidebar />
     </div>
   );
 }
